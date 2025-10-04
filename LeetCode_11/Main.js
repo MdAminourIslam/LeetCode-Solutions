f659b8cd11508465157c23560600a45583f960d9
Md. Aminour Islam <mdaminour002@gmail.com>
2025-10-04 21:08:07 +0600

    Modify Leetcode 11 - Container With Most Water

--- LeetCode_11/Main.js
+++ LeetCode_11/Main.js
@@ -6,7 +6,7 @@ var maxArea = function(height) {
     let answer = 0;
     let lo = 0, hi = height.length - 1;
     while (lo <= hi) {
-        answer = Math.max(answer, (hi - lo + 1) * Math.min(height[lo], height[hi]));
+        answer = Math.max(answer, (hi - lo) * Math.min(height[lo], height[hi]));
         
         if (height[lo] <= height[hi]) {
             lo += 1;
