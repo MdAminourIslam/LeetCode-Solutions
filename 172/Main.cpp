e1bc1cc7c334e6668b150381394042ba2a170041
Md. Aminour Islam <mdaminour002@gmail.com>
2026-05-20 23:59:16 +0600

    Solve 172. Factorial Trailing Zeroes

---
+++ 172/Main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int answer = 0;
+        for (int i = 5; i <= n; i *= 5) {
+            answer += n / i;
+        }
+        return answer;
+    }
+};
